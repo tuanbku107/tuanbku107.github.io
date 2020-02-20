cb50c0867a276f28bf7fc9c987032f69ca403542
tuanbku107 <tuanbku107@gmail.com>
2020-02-20 15:06:58 +0700

    make it better

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "facebook_api_key": "605512303625867",
+    "facebook_api_secret": "a61bc048e461be21b12869274ff5ee29",
+    "callback_url": "/auth/facebook/callback",
+    "use_database": false,
+    "host": "localhost",
+    "username": "root",
+    "password": "",
+    "database": "Database Name"
+}
