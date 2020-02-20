97380d7c88ca2a7f4c461f5d87ccb9cb23f617c2
tuanbku107 <tuanbku107@gmail.com>
2020-02-20 15:23:50 +0700

    update

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ app.get('/auth/facebook', passport.authenticate('facebook'));
 app.get('/auth/facebook/callback',
     passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }),
     function (req, res) {
+        console.log('check callback::');
         res.redirect('/');
     });
 
